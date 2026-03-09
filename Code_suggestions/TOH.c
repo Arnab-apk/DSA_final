ada008dd7391c2cb67ca7639afd4788b0cf8ed07
Mr.Invictus <arnabmandal261@gmail.com>
2026-03-09 23:56:34 +0530

    Fix function name in Tower of Hanoi implementation for consistency

--- Code_suggestions/TOH.c
+++ Code_suggestions/TOH.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-void tower_of_hanoi(int n,char source,char aux,char dest)
+void toh(int n,char source,char aux,char dest)
 {
     if(n==1){
         printf("Move disk 1 from %c to %c\n",source,dest);
         return;
     }
     //move n-1 disk from source to auxillary
-    tower_of_hanoi(n-1,source,dest,aux);
+    toh(n-1,source,dest,aux);
     //move nth disk from source to dest
     printf("Move disk %d from %c to %c\n",n ,source ,dest);
     //move n-1 disk from aux to dest
-    tower_of_hanoi(n-1,aux,source,dest);
+    toh(n-1,aux,source,dest);
 }
 
 int main()
